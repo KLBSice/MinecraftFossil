b29adfca793f1c242662e3c0477881ca0fd9393c
Murak549 <51825158+Murak549@users.noreply.github.com>
2021-08-02 21:36:34 +0900

    Update GetTime.java

--- src/main/java/Murak/MurakFossil/Commands/GetTime.java
+++ src/main/java/Murak/MurakFossil/Commands/GetTime.java
@@ -17,7 +17,7 @@ public class GetTime implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player)sender;
-        p.sendMessage(dtf.format(now));
+        p.sendMessage("current year, date and time is" + dtf.format(now));
         return true;
     }
 }
